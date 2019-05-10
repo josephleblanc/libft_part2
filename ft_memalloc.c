f5e4759e416c0c844235d2d02d424bdbb124dfc4
josephleblanc <joseph.t.leblanc@protonmail.com>
2019-05-10 09:59:05 -0700

    Wrote and tested memalloc.c, it also includes a test which has been commented out

---
+++ ft_memalloc.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+// #include <stdio.h>       // These three includes are for
+// #include <strings.h>     // tests
+// #include <unistd.h>
+
+void *ft_memalloc(size_t n)
+{
+    void *vp;
+
+    if (vp = malloc(n))
+    {
+        bzero(vp, n); // change to ft_bzero
+        return vp;
+    }
+    return NULL;
+}
+
+
+// Test
+//int main(void)
+//{
+//    size_t n;
+//    void *input;
+//    char *s;
+//    int i;
+//
+//    i = -1;
+//    n = 6;
+//
+//    if (input = ft_memalloc(n))
+//    {
+//        while (++i < n)
+//        {
+//            s = (char*)input;
+//            *s = *s + 'a';
+//            write(1, s++, 1);
+//            input++;
+//        }
+//    }
+//    else
+//        printf("memalloc returned NULL\n");
+//    printf("\n");
+//    return 0;
+//
+//}
