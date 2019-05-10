d55e1895f82060b4b5a0f8709cafc9083e691c0d
josephleblanc <joseph.t.leblanc@protonmail.com>
2019-05-10 11:19:20 -0700

    Add ft_striteri.c and eponymous function, specced but untested

---
+++ ft_striteri.c
@@ -0,0 +1,26 @@
+// Description
+// Applies the function f to each character of the string passed 
+// as argument, and passing its index as first argument. Each 
+// character is passed by address to f to be modified if
+// necessary.
+//
+// Param #1
+// The string to iterate.
+//
+// Param #2
+// The function to apply to each character of s and its index.
+//
+// Return value
+// None
+//
+// Libc functions
+// None
+
+void    ft_striteri(char *s, void(*f)(unsigned int, char*))
+{
+    unsigned int i;
+    
+    index = 0;
+    while (s)
+        f(i++, s++);
+}
